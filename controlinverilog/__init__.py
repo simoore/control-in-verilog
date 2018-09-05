ae07cd32d44e3fcb8dae8f515f4ec8845d478144
Steven Moore <steven.ian.moore@gmail.com>
2018-09-05 17:00:07 +1000

    Added a nonlinear function class.

--- controlinverilog/__init__.py
+++ controlinverilog/__init__.py
@@ -2,6 +2,7 @@ from .decimator import Decimator
 from .integrator import Integrator
 from .lti_system import LtiSystem
 from .lti_verilog import LtiVerilog
+from .nonlinear_function import NonlinearFunction
 from .mechatronics import norm_hinf_continuous
 from .dds import DDS
 
